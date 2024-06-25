0eefae100bba53622daddbb5c5c654c617ba932b
Varun Kumar <varuntheace@gmail.com>
2024-06-25 21:24:14 +0530

    Updated a few modules

---
+++ pharma/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Dealer
+from .models import Employee
+from .models import Customer
+from .models import Medicine
+from .models import Purchase
+
+admin.site.register(Dealer)
+admin.site.register(Employee)
+admin.site.register(Customer)
+admin.site.register(Medicine)
+admin.site.register(Purchase)
